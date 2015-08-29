ede60c04fbf800a2bb53d501d170a8067becdb52
Samar Panda <samar.panda84@gmail.com>
2015-08-29 15:13:27 +0530

    initial commit

---
+++ app/app.js
@@ -0,0 +1,4 @@
+var application = require("application");
+application.mainModule = "./login/login";
+application.cssFile = "./app.css";
+application.start();
---
+++ app/login/login.js
@@ -0,0 +1,6 @@
+var pagesModule = require('ui/page');
+var labelModule = require('ui/label');
+
+function pageLoaded(args) {
+    console.log("Page Loaded");
+}
